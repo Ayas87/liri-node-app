fe159ab5f977f432d62c0634e1092131fe2aca69
Ayas87 <randylam87@gmail.com>
2017-05-20 11:38:31 -0700

    basic hw complete

--- liri.js
+++ liri.js
@@ -1,21 +1,17 @@
 let keys = require('./keys.js');
+let fs = require('fs');
 let command = process.argv[2];
 let input = process.argv[3];
-
-let Twitter = require('twitter');
-let client = new Twitter(keys.twitterKeys);
-let options = {
-    screen_name: 'RandyLam87',
-    count: 20,
-    trim_user: 1
-};
-
-var request = require('request');
-var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&r=json";
-
-
+//Twitter
 function getTwitter() {
-    // This will show your last 20 tweets and when they were created at in your terminal/bash window.
+    let Twitter = require('twitter');
+    let client = new Twitter(keys.twitterKeys);
+    let options = {
+        screen_name: 'RandyLam87',
+        count: 20,
+        trim_user: 1
+    };
+
     client.get('statuses/user_timeline', options, function (error, tweets, response) {
         for (i = 0; i < tweets.length; i++) {
             console.log(tweets[i].text);
@@ -23,29 +19,31 @@ function getTwitter() {
     });
 }
 
+//Spotify
 function getSpotify(input) {
-    var spotify = require('spotify');
+    let spotify = require('spotify');
     spotify.search({
         type: 'track',
         query: input
     }, function (err, data) {
-        // if (err) {
-        //     console.log('Error occurred: ' + err);
-        //     return;
-        // }
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
         for(i=0; i<data.tracks.items.length; i++){
             console.log('Artist: ' + data.tracks.items[i].artists[0].name);
             console.log('Song Name: ' + data.tracks.items[i].name);
             console.log('Song Name: ' + data.tracks.items[i].preview_url);
             console.log('Album: ' + data.tracks.items[i].album.name + '\n');
         }
-        // console.log(data.tracks.items);
     });
 }
 
-
-
+//IMDB
 function getMovieInfo() {
+    let request = require('request');
+    let queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&r=json";
+
     request(queryUrl, function (error, response, body) {
         if (error) {
             return console.log(error);
@@ -64,20 +62,28 @@ function getMovieInfo() {
         }
     });
 }
+
+//Do What It Says
+function getDoWhatItSays(){
+    fs.readFile('random.txt','utf8', function(error, data){
+        let newData = data.split(',');
+        liri(newData[0],newData[1])
+    });
+}
+
+//LIRI
 function liri (command, input) {
     if (command === 'my-tweets') {
         getTwitter();
     } else if (command === 'spotify-this-song') {
-        console.log('spotify');
         getSpotify(input);
     } else if (command === 'movie-this') {
         getMovieInfo();
     } else if (command === 'do-what-it-says') {
-        console.log('reads txt');
+        getDoWhatItSays();
     } else {
         console.log('Invalid command \n The commands are: \n my-tweets \n spotify-this-song \n movie-this \n do-what-it-says');
     }
 }
 
 liri(command, input);
-
